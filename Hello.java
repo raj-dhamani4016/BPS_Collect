97640f59721e142d20319b2dcd8004f5a364632d
raj-dhamani4016 <tech.srjsharma@gmail.com>
2021-04-06 18:16:31 +0530

    third commit

--- Hello.java
+++ Hello.java
@@ -2,7 +2,10 @@ class Hello{
  public static void main(String args[]){
 
   for(int i=0;i<=10;i++){
-     System.out.println("Hello User , Welcome  to raj store fff Jenkins world");
+     System.out.println("Hello User , Welcome to Jenkins world");
+    }
+for(int i=0;i<=10;i++){
+     System.out.println("Hello User , Welcome to Jenkins world");
     }
  }
 }
